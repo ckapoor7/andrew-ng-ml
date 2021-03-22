e78770ad5a5dadb11b2ab4bece5f5128b0b1b8e0
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2021-03-22 20:58:33 +0530

    implemented the cost function

--- week3/ex2/costFunction.m
+++ week3/ex2/costFunction.m
@@ -10,23 +10,7 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-
-
-
-
-
-
-
-% =============================================================
+hypothesis = sigmoid(X*theta);
+J = (1/m)*sum(-y'*log(hypothesis)-(1-y)'*log(1-hypothesis)); %Use the formula derived
 
 end
