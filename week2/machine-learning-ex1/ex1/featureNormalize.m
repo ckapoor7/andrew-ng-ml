f597d620f5a9c8df4a6294cc792c71c6ad2a98e3
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2021-03-22 20:40:26 +0530

    removed redundancies

--- week2/machine-learning-ex1/ex1/featureNormalize.m
+++ week2/machine-learning-ex1/ex1/featureNormalize.m
@@ -12,31 +12,4 @@ sigma = std(X);
 X = (X-mu)./sigma;
 X_norm = X;
 
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: First, for each feature dimension, compute the mean
-%               of the feature and subtract it from the dataset,
-%               storing the mean value in mu. Next, compute the 
-%               standard deviation of each feature and divide
-%               each feature by it's standard deviation, storing
-%               the standard deviation in sigma. 
-%
-%               Note that X is a matrix where each column is a 
-%               feature and each row is an example. You need 
-%               to perform the normalization separately for 
-%               each feature. 
-%
-% Hint: You might find the 'mean' and 'std' functions useful.
-%       
-
-
-
-
-
-
-
-
-
-% ============================================================
-
 end
