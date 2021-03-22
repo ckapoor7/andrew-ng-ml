1d683f41f55d696b03120f4e70a018193ac5ac42
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2021-03-22 20:41:37 +0530

    visualise model

--- week3/ex2/plotData.m
+++ week3/ex2/plotData.m
@@ -4,11 +4,12 @@ function plotData(X, y)
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
 % Create New Figure
-figure; hold on;
+figure;
+hold on;
 
 % Find Indices of Positive and Negative Examples
 pos = find(y==1); neg = find(y == 0);
-% Plot Examples
+
 plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, ...
      'MarkerSize', 7);
 plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...
